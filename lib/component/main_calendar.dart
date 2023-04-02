4bb6cce8d9d36819bec765e7bb97687c2b86e4fd
ZanacXevious <ckongmario21@outlook.com>
2023-04-02 10:02:10 -0600

    zanac_calender_005

--- lib/component/main_calendar.dart
+++ lib/component/main_calendar.dart
@@ -2,9 +2,21 @@ import 'package:flutter/material.dart';
 import 'package:table_calendar/table_calendar.dart';
 
 class MainCalendar extends StatelessWidget {
+  final OnDaySelected onDaySelected;
+  final DateTime selectedDAte;
+
+  MainCalendar(
+      {Key? key, required this.onDaySelected, required this.selectedDAte})
+      : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return TableCalendar(
+      onDaySelected: onDaySelected,
+      selectedDayPredicate: (date) =>
+          date.year == selectedDAte.year &&
+          date.month == selectedDAte.month &&
+          date.day == selectedDAte.day,
       firstDay: DateTime(1800, 1, 1),
       lastDay: DateTime(2100, 1, 1),
       focusedDay: DateTime.now(),
