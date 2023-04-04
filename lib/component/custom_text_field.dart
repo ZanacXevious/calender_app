e5eddb577418345a2387d7aa813ed4db1be76468
ZanacXevious <ckongmario21@outlook.com>
2023-04-03 20:25:38 -0600

    zanac_calender_014

---
+++ lib/component/custom_text_field.dart
@@ -0,0 +1,26 @@
+import 'package:calendar_scheduler/const/colors.dart';
+import 'package:flutter/material.dart';
+
+class CustomTextField extends StatelessWidget {
+  final String label;
+
+  const CustomTextField({Key key = const Key(''), required this.label})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(
+          label,
+          style: TextStyle(
+            color: PRIMARY_COLOR,
+            fontWeight: FontWeight.w600,
+          ),
+        ),
+        TextField(),
+      ],
+    );
+  }
+}
