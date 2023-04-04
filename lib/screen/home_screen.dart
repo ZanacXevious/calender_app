142df624010f41a25c4e1005fa004792d02a580d
ZanacXevious <ckongmario21@outlook.com>
2023-04-03 19:37:40 -0600

    zanac_calender_013

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -1,4 +1,6 @@
+import 'package:calendar_scheduler/component/schedule_bottom_sheet.dart';
 import 'package:calendar_scheduler/component/today_banner.dart';
+import 'package:calendar_scheduler/const/colors.dart';
 import 'package:flutter/material.dart';
 import 'package:calendar_scheduler/component/main_calendar.dart';
 import 'package:table_calendar/table_calendar.dart';
@@ -21,6 +23,17 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: PRIMARY_COLOR,
+        onPressed: () {
+          showModalBottomSheet(
+            context: context,
+            isDismissible: true,
+            builder: (context) => ScheduleBottomSheet(),
+          );
+        },
+        child: Icon(Icons.add),
+      ),
       body: SafeArea(
         child: Column(
           children: [
